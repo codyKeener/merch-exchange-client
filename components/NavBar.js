0eabde4d76236a196cc001584e2bf7b5b189b208
Cody Keener <36174154+codyKeener@users.noreply.github.com>
2025-02-24 20:34:59 -0600

    Set up Firebase auth, updated navbar, and added some basic styling

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,29 +6,54 @@ import {
   Container,
   Nav,
   Button,
+  Image,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" class="navbar">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>
+            <Image
+              src="/merch-exchange-logo.png"
+              alt="Merch Exchange Logo"
+              height="40px"
+            />
+          </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav
+            className="me-auto"
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
-            </Link>
-            <Button variant="danger" onClick={signOut}>
-              Sign Out
-            </Button>
+            <div style={{
+              display: 'flex',
+            }}
+            >
+              <Link passHref href="/bands">
+                <Nav.Link className="navbar navlink">Bands</Nav.Link>
+              </Link>
+              <Link passHref href="/products">
+                <Nav.Link className="navbar navlink">Products</Nav.Link>
+              </Link>
+            </div>
+            <div style={{
+              display: 'flex',
+              marginLeft: 'auto',
+            }}
+            >
+              <Button variant="danger" onClick={signOut}>
+                Sign Out
+              </Button>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
